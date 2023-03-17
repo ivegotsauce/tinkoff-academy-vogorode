75c7d190a5d178f4329b00f806e7d24ee35fe0cc
Alexander Sharipov <alexsharipov1@gmail.com>
2023-03-17 22:14:09 +0300

    Solution for Homework 5 added

---
+++ landscape/src/main/resources/db/2023-03-16--01-create-table-client.sql
@@ -0,0 +1,10 @@
+create table if not exists client
+(
+    id            uuid primary key default uuid_generate_v4(),
+    user_type     text,
+    login         varchar(42),
+    email         text,
+    phone_number  text,
+    creation_date date,
+    update_date   date
+);
